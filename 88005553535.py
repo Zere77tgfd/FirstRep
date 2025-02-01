b7092b90c47cc7e3118092d079c722cb2759c5d6
Zere77tgfd <rampapura@gmail.com>
2025-02-01 19:46:43 +0300

    1st ccommit

---
+++ 88005553535.py
@@ -0,0 +1,14 @@
+def ask_password():
+    for i in range(3):
+        a = input("Введите пароль: ")
+        if a == password:
+            print('Правильный пароль')
+            break
+        else:
+            print('Неверно')
+            if i == 2:
+                print("Повторите попытку позже")
+
+
+password = "123456789"
+ask_password()
